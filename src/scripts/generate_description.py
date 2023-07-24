23280b577dfea445247c1e2a9d123fa91e147c04
Ewuzie Chima Paul <chima@vida.place>
2023-07-24 11:46:27 +0200

    fix: creating runnable scripts

--- src/scripts/generate_description.py
+++ src/scripts/generate_description.py
@@ -1,19 +1,15 @@
 # Imports
 import os
+import sys
 from dotenv import load_dotenv
 
-load_dotenv()
-
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-%matplotlib inline
 
 # LangChain imports
 from langchain.agents import create_pandas_dataframe_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain.chains.summarize import load_summarize_chain
+# from langchain.chat_models import ChatOpenAI
+# from langchain.agents.agent_types import AgentType
+# from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders import UnstructuredExcelLoader, UnstructuredFileLoader
 from langchain.llms import OpenAI
@@ -22,10 +18,29 @@ from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain import PromptTemplate
 
+load_dotenv('../../.env.production')
+print(os.getcwd())
+
+if len(sys.argv) < 2:
+    print("You have to specify the name of the dataset \nExiting...")
+    exit()
+
+elif len(sys.argv) > 2:
+    print("Function takes only one argument (name of dataset) but more than one argument found \nExiting...")
+    exit()
+
+else:
+    file_path = f"../datasets/{sys.argv[1]}"
+    if os.path.exists(file_path):
+        print(f"The dataset {file_path} was found \nProceeding...")
+    else:
+        print(f"The path {file_path} to the the dataset does not exist \nExiting")
+        exit()
 
 api_key = os.getenv("OPENAI_API_KEY")
 # Display all columns from dataset
 pd.set_option('display.max_columns', None)
+llm = OpenAI(temperature=0, openai_api_key = api_key)
 
 # function to engineer prompt with column names only
 def create_prompt_col(col):
@@ -40,18 +55,15 @@ def create_prompt_col(col):
 # makes prompt from column names and 5 first rows
 def create_prompt(col, row):
     prompt = PromptTemplate(
-    input_variables=["columns", "rows"],
-    template = "Generate a summary about this dataset based on {columns} and {rows} information"
+        input_variables=["columns", "rows"],
+        template = "Generate a summary about this dataset based on {columns} and {rows} information"
     )
     query = prompt.format(columns = list(col), rows = row)
     return query
 
 # function to generate description with pandas agent
 def generate_desc_pd_agent(df, question):
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0),
-                                         df,
-                                         verbose=True
-                                         )
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), df, verbose=True)
     description = agent.run(question)
     return description
 
@@ -61,10 +73,20 @@ def gen_desc_gpt(query):
     return description
 
 # function to generaate description with Retrieval QA
-def gen_desc_rQA(file, api_k, llm, query):
-    df = pd.read_excel(file)
-    query_gpt = create_prompt_col(df.columns)
-    loader = UnstructuredFileLoader(file)
+def gen_desc_rQA(dataset_path: str, generate_tags: bool) -> str:
+
+    if dataset_path.endswith('.csv'):
+        data_file = pd.read_csv(dataset_path)
+    
+    elif dataset_path.endswith('.xlsx'):
+        data_file = pd.read_excel(dataset_path)
+    
+    else:
+        print("Please provide a valid dataset in the csv or excel format")
+        return
+    
+    query = "Give an elaborate description of the dataset"
+    loader = UnstructuredFileLoader(dataset_path)
     doc = loader.load()
     
     # split docs
@@ -75,19 +97,25 @@ def gen_desc_rQA(file, api_k, llm, query):
     # print(f"Now you have {len(docs[:4])} documents that have an average of {total_num_char / len(docs):,.0f} characters")
     # embeddings
     # creating vectors db
-    embeddings = OpenAIEmbeddings(openai_api_key=api_k)
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
     docsearch = FAISS.from_documents(docs[:4], embeddings)
     
     # Create retrieval QA engine
     qa = RetrievalQA.from_chain_type(llm = llm,
                                      chain_type = "stuff",
                                      retriever = docsearch.as_retriever(),
-                                     verbose = True)
+                                     verbose = True
+    )
     
     try:                     
         description = qa.run(query)
     except Exception:
         print("Error message:")
+        query_gpt = create_prompt_col(data_file.columns)
         description = gen_desc_gpt(query_gpt)
     finally:
         return description
+    
+if __name__ == '__main__':
+    description = gen_desc_rQA(file_path, api_key)
+    print(description)
