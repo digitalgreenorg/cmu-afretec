8a7921313a777efd71b085d9069172ce429fd1cf
Ewuzie Chima Paul <chima@vida.place>
2023-07-29 16:22:47 +0200

    feat: Adding the tags from the description

--- src/scripts/extract_keyword.py
+++ src/scripts/extract_keyword.py
@@ -8,19 +8,6 @@ from dotenv import load_dotenv
 from typing import List
 import pandas as pd
 
-# LangChain imports
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.chat_models import ChatOpenAI
-from langchain.agents.agent_types import AgentType
-from langchain.chains.summarize import load_summarize_chain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import UnstructuredExcelLoader, UnstructuredFileLoader
-from langchain.llms import OpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains import RetrievalQA
-from langchain import PromptTemplate
-
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
